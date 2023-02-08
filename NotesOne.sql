56aca4f8a4a5b86e51753ba70406ae3befef1d35
Thushan Ranasinghe <88255435+thushanr5@users.noreply.github.com>
2023-02-08 16:28:52 -0500

    Update NotesOne.sql

--- NotesOne.sql
+++ NotesOne.sql
@@ -2,4 +2,4 @@ USE sakila; --Name of the table
 
 SELECT * FROM sakila.actor 
 -- WHERE actor_id = 2 (WHERE CLAUSE)
-ORDER BY first_name;  (ALPHABETICAL ORDERING)
+ORDER BY first_name; --(ALPHABETICAL ORDERING)
