80c44b09b5a112058d53c29130898f30d5472bf4
Thushan Ranasinghe <88255435+thushanr5@users.noreply.github.com>
2023-02-08 16:40:45 -0500

    Update NotesOne.sql

--- NotesOne.sql
+++ NotesOne.sql
@@ -6,3 +6,6 @@ ORDER BY first_name; --(ALPHABETICAL ORDERING)
 
 SELECT first_name, last name FROM 
 actor WHERE actor_id = 2 + 10; --The 2 + 10 is an expression which can be evaluated in SQL
+
+USE world;
+SELECT CountryCode, Language, IsOfficial, Percentage, Percentage * 10 AS 'New Percentage' FROM world.countrylanguage;
