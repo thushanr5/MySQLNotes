bfd1a757deb7901e05f73fc1aa7aa260d1a8eb27
Thushan Ranasinghe <88255435+thushanr5@users.noreply.github.com>
2023-02-08 16:32:08 -0500

    Update NotesOne.sql

--- NotesOne.sql
+++ NotesOne.sql
@@ -3,3 +3,6 @@ USE sakila; --Name of the table
 SELECT * FROM sakila.actor 
 -- WHERE actor_id = 2 (WHERE CLAUSE)
 ORDER BY first_name; --(ALPHABETICAL ORDERING)
+
+SELECT first_name, last name FROM 
+actor WHERE actor_id = 2 + 10; --The 2 + 10 is an expression which can be evaluated in SQL
